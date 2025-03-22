0550d22f8e6b6d191ae725ecaec92b75344aa630
Nazima <ergeshovanazima2@gmail.com>
2025-03-23 02:42:12 +0900

    두 번째 자바 프로그램 추가

--- Main.java
+++ Main.java
@@ -3,4 +3,11 @@ public class Main {
         System.out.println("Hello, GitHub!");
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        // 기존 코드에 추가한 내용
+        System.out.println("Hello, World!");
+        System.out.println("두 번째 자바 프로그램!");
+    }
+}
 
